b86aae9a15f030af733c14f261864967bd3d6943
Divinejay-001 <divinegiftjohnson21@gmail.com>
2025-02-21 20:32:46 -0800

    push

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -97,7 +97,7 @@ object-fill rounded-md hover:scale-105 duration-500 transition-all'/>
                  <button className='text-center mt-10
                  cursor-pointer bg-primary text-white
                  py-1 px-5 rounded-md'>
-                <a href='/womensWear'>View All </a>    
+                <Link to='/womensWear'>View All </Link>    
                     </button>   
                 </div>
             </div>
